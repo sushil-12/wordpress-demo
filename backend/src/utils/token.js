6cf01507883c8421f8b3aa54405893ab1df2b3a0
nbh174 <hi@munishsharma.in>
2024-03-03 21:03:16 +0530

    pushed

--- backend/src/utils/token.js
+++ backend/src/utils/token.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const credentials = require('./cred.json');
 
 // Replace with the code you received from Google
-const code = '4/0AeaYSHClfX-o81fL8fmBkYGtmD_ay1DyGeXGvMLoTf6oT38P_J3vhlhc198M44XeciH4Lw';
+const code = '4/0AeaYSHAuKveYKXFj6S3MEqxuYiZ-OZaTsnHuJLx2ryh6xMwt5zQTFcf_9fDBp9_EHoS9sw';
 const { client_secret, client_id, redirect_uris } = credentials.web;
 const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
